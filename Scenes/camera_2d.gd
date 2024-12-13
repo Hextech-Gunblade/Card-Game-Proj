aec80536209846dff3c29449a429f1a94931c89b
IPAO69 <pongpon.torungruengkit@gmail.com>
2024-12-13 21:37:19 +0700

    Update camera_2d.gd
    add comment

--- Scenes/camera_2d.gd
+++ Scenes/camera_2d.gd
@@ -1,22 +1,20 @@
 extends Camera2D
-
+#Setting Zoom Speed and Move Speed
 @export var zoomSpeed: float = 10
 @export var moveSpeed: float = 1000
-
+#Declare Variable
 var zoomTarget: Vector2
 
-var dragStartMousePos = Vector2.ZERO
-var dragStartCameraPoos = Vector2.ZERO
-var isDragging : bool = false;
 
+#Varible for Smooth Zooming
 func _ready():
 	zoomTarget = zoom
-
+#Declare delta in all function
 func _process(delta):
 	Zoom(delta)
 	SimplePan(delta)
 	ClickAndDrag(delta)
-
+#Zoom Function
 func Zoom(delta):
 	if Input.is_action_just_pressed("zoom_in"):
 		zoomTarget *= 1.1
@@ -24,7 +22,7 @@ func Zoom(delta):
 		zoomTarget *= 0.9
 
 	zoom = zoom.slerp(zoomTarget, zoomSpeed * delta)
-
+#Camera Pan Function
 func SimplePan(delta):
 	var moveAmount=Vector2.ZERO
 	if Input.is_action_pressed("camera_move_left"):
@@ -40,5 +38,5 @@ func SimplePan(delta):
 	pass
 
 func ClickAndDrag(delta):
-	# Add functionality for click-and-drag here
+	#WIP
 	pass
